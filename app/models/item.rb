ccac8585ef377baffbfa3ee40d758431704c6434
shikadayuta <sikada1218@gmail.com>
2021-09-03 15:28:25 +0900

    コードの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -25,4 +25,3 @@ class Item < ApplicationRecord
   validates :item_shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :item_scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
 end
-"can't be blank"
